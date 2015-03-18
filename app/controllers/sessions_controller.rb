08cb2b7e47bb1b6d3a130b108953de5af313064f
Amy Johnson and Mike Harris <mikeharris.jmh2@yahoo.com>
2015-03-18 14:02:13 -0700

    Add files, create sign in, log in, and log out feature

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      flash[:notice]= "You've been logged in"
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:alert] = "There was a problem logging in"
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You've been logged out successfully"
+    redirect_to users_path
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def index
+  @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Site!"
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:alert] = "There was a problem creating your account. Please try again."
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
