4485e2eded0bf6e7d2f82a2325a13f399050fb6d
Amy Johnson and Mike Harris <mikeharris.jmh2@yahoo.com>
2015-03-18 17:38:13 -0700

    Add ability to list photos and redirect to correct user pages

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,8 +5,14 @@ class PhotosController <ApplicationController
   end
 
   def create
-    @photo = Photo.find(params[:user_id])
-    @user = session[:user_id] = @user.id
+    @user = User.find(params[:current_user_id])
+    @photo = @user.photos.new(photo_params)
+    if @photo.save
+      flash[:notice] = "Photo successfully uploaded"
+      redirect_to user_photo(@photo)
+    else
+      render :new
+    end
   end
 
   def photo_params
